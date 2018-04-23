ad736735bff75097bce8c6e0b5d1265ec1556805
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/GridGroupSelect/MainWindow.xaml.vb
@@ -0,0 +1,12 @@
+﻿Imports System.Windows
+
+Namespace dxSample
+    Partial Public Class MainWindow
+        Inherits Window
+
+        Public Sub New()
+            InitializeComponent()
+            DataContext = SampleDataRow.CreateRows()
+        End Sub
+    End Class
+End Namespace
