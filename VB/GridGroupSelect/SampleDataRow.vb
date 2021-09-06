a4506cb7ede0f1d76926995d68ab9a295a160903
DevExpressExampleBot <fakeEmail>
2021-09-06 22:37:38 +0300

    Source auto update [skip ci]

--- VB/GridGroupSelect/SampleDataRow.vb
+++ VB/GridGroupSelect/SampleDataRow.vb
@@ -7,58 +7,58 @@ Imports System.Threading.Tasks
 Imports System.Windows
 
 Namespace dxSample
-    Friend Class SampleDataRow
-        Inherits DependencyObject
+	Friend Class SampleDataRow
+		Inherits DependencyObject
 
-        Public Property Id() As Integer
-            Get
-                Return DirectCast(GetValue(IdProperty), Integer)
-            End Get
-            Set(ByVal value As Integer)
-                SetValue(IdProperty, value)
-            End Set
-        End Property
-        Public Property Group() As String
-            Get
-                Return DirectCast(GetValue(GroupProperty), String)
-            End Get
-            Set(ByVal value As String)
-                SetValue(GroupProperty, value)
-            End Set
-        End Property
-        Public Property Name() As String
-            Get
-                Return DirectCast(GetValue(NameProperty), String)
-            End Get
-            Set(ByVal value As String)
-                SetValue(NameProperty, value)
-            End Set
-        End Property
-        Public Property HasFlag() As Boolean
-            Get
-                Return DirectCast(GetValue(HasFlagProperty), Boolean)
-            End Get
-            Set(ByVal value As Boolean)
-                SetValue(HasFlagProperty, value)
-            End Set
-        End Property
+		Public Property Id() As Integer
+			Get
+				Return DirectCast(GetValue(IdProperty), Integer)
+			End Get
+			Set(ByVal value As Integer)
+				SetValue(IdProperty, value)
+			End Set
+		End Property
+		Public Property Group() As String
+			Get
+				Return DirectCast(GetValue(GroupProperty), String)
+			End Get
+			Set(ByVal value As String)
+				SetValue(GroupProperty, value)
+			End Set
+		End Property
+		Public Property Name() As String
+			Get
+				Return DirectCast(GetValue(NameProperty), String)
+			End Get
+			Set(ByVal value As String)
+				SetValue(NameProperty, value)
+			End Set
+		End Property
+		Public Property HasFlag() As Boolean
+			Get
+				Return DirectCast(GetValue(HasFlagProperty), Boolean)
+			End Get
+			Set(ByVal value As Boolean)
+				SetValue(HasFlagProperty, value)
+			End Set
+		End Property
 
-        Public Shared ReadOnly HasFlagProperty As DependencyProperty = DependencyProperty.Register("HasFlag", GetType(Boolean), GetType(SampleDataRow), New PropertyMetadata(False))
-        Public Shared ReadOnly NameProperty As DependencyProperty = DependencyProperty.Register("Name", GetType(String), GetType(SampleDataRow), New PropertyMetadata(""))
-        Public Shared ReadOnly GroupProperty As DependencyProperty = DependencyProperty.Register("Group", GetType(String), GetType(SampleDataRow), New PropertyMetadata(""))
-        Public Shared ReadOnly IdProperty As DependencyProperty = DependencyProperty.Register("Id", GetType(Integer), GetType(SampleDataRow), New PropertyMetadata(0))
+		Public Shared ReadOnly HasFlagProperty As DependencyProperty = DependencyProperty.Register("HasFlag", GetType(Boolean), GetType(SampleDataRow), New PropertyMetadata(False))
+		Public Shared ReadOnly NameProperty As DependencyProperty = DependencyProperty.Register("Name", GetType(String), GetType(SampleDataRow), New PropertyMetadata(""))
+		Public Shared ReadOnly GroupProperty As DependencyProperty = DependencyProperty.Register("Group", GetType(String), GetType(SampleDataRow), New PropertyMetadata(""))
+		Public Shared ReadOnly IdProperty As DependencyProperty = DependencyProperty.Register("Id", GetType(Integer), GetType(SampleDataRow), New PropertyMetadata(0))
 
-        Public Shared Function CreateRows() As IList(Of SampleDataRow)
-            Dim result As IList(Of SampleDataRow) = New List(Of SampleDataRow)()
-            For i As Integer = 0 To 99
-                result.Add(New SampleDataRow() With { _
-                    .Id = i, _
-                    .Group = String.Format("group {0}", i Mod 10), _
-                    .Name = String.Format("name {0}", i Mod 3), _
-                    .HasFlag = (i Mod 3 = 0) _
-                })
-            Next i
-            Return result
-        End Function
-    End Class
+		Public Shared Function CreateRows() As IList(Of SampleDataRow)
+			Dim result As IList(Of SampleDataRow) = New List(Of SampleDataRow)()
+			For i As Integer = 0 To 99
+				result.Add(New SampleDataRow() With {
+					.Id = i,
+					.Group = String.Format("group {0}", i Mod 10),
+					.Name = String.Format("name {0}", i Mod 3),
+					.HasFlag = (i Mod 3 = 0)
+				})
+			Next i
+			Return result
+		End Function
+	End Class
 End Namespace
